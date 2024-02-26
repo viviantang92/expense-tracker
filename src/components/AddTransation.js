b0d958a1822abb1dd26abe36a00f05954b7d5916
viviantang92 <vivian.tang@hotmail.se>
2024-02-26 23:39:04 +0100

    UI Completed

---
+++ src/components/AddTransation.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const AddTransation = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+  return (
+    <>
+      <h3>Add new transation</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">
+            Amount <br /> (negative - expense, positive - income)
+          </label>
+
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  );
+};
+
+export default AddTransation;
